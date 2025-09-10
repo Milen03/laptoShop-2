28ba54f35ae9d2de6430c47cb5037e6b2afaeb72
Milen03 <milen.atanasovv03@gmail.com>
2025-09-10 19:31:17 +0300

    See only Admin

--- laptop2/resources/views/layouts/navigation.blade.php
+++ laptop2/resources/views/layouts/navigation.blade.php
@@ -20,41 +20,42 @@
                 </a>
 
                 @auth
-                    <a href="{{ route('laptops.create') }}" class="text-gray-700 hover:text-indigo-600 flex items-center">
-                        <i class="fas fa-plus mr-1"></i> Добави лаптоп
-                    </a>
+                <a href="{{ route('laptops.create') }}" class="text-gray-700 hover:text-indigo-600 flex items-center">
+                    <i class="fas fa-plus mr-1"></i> Добави лаптоп
+                </a>
 
 
-                    <!-- Dropdown -->
-                    <div class="relative" x-data="{ open: false }">
-                        <button @click="open = !open" class="flex items-center text-gray-700 hover:text-indigo-600 focus:outline-none">
-                            <i class="fas fa-user-circle mr-1 text-xl"></i>
-                            <span>{{ Auth::user()->name }}</span>
-                            <i class="fas fa-chevron-down ml-1 text-sm"></i>
-                        </button>
+                <!-- Dropdown -->
+                <div class="relative" x-data="{ open: false }">
+                    <button @click="open = !open" class="flex items-center text-gray-700 hover:text-indigo-600 focus:outline-none">
+                        <i class="fas fa-user-circle mr-1 text-xl"></i>
+                        <span>{{ Auth::user()->name }}</span>
+                        <i class="fas fa-chevron-down ml-1 text-sm"></i>
+                    </button>
 
-                        <!-- Dropdown menu -->
-                        <div x-show="open" @click.away="open = false" 
-                             class="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg z-50">
-                            
-                            <a href="{{ url('/admin') }}" class="block px-4 py-2 text-gray-700 hover:bg-gray-100">
-                                <i class="fas fa-cogs mr-2"></i> Admin
-                            </a>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit" class="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
-                                    <i class="fas fa-sign-out-alt mr-2"></i> Изход
-                                </button>
-                            </form>
-                        </div>
+                    <!-- Dropdown menu -->
+                    <div x-show="open" @click.away="open = false"
+                        class="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg z-50">
+                        @if(Auth::user()->canAccessPanel(app(\filament\Panel::class)))
+                        <a href="{{ url('/admin') }}" class="block text-gray-700 hover:text-indigo-600">
+                            <i class="fas fa-cogs mr-1"></i> Admin
+                        </a>
+                        @endif
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button type="submit" class="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
+                                <i class="fas fa-sign-out-alt mr-2"></i> Изход
+                            </button>
+                        </form>
                     </div>
+                </div>
                 @else
-                    <a href="{{ route('login') }}" class="text-gray-700 hover:text-indigo-600 flex items-center">
-                        <i class="fas fa-sign-in-alt mr-1"></i> Вход
-                    </a>
-                    <a href="{{ route('register') }}" class="ml-3 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 flex items-center">
-                        <i class="fas fa-user-plus mr-1"></i> Регистрация
-                    </a>
+                <a href="{{ route('login') }}" class="text-gray-700 hover:text-indigo-600 flex items-center">
+                    <i class="fas fa-sign-in-alt mr-1"></i> Вход
+                </a>
+                <a href="{{ route('register') }}" class="ml-3 px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 flex items-center">
+                    <i class="fas fa-user-plus mr-1"></i> Регистрация
+                </a>
                 @endauth
             </div>
 
@@ -79,10 +80,11 @@
         <a href="{{ route('laptops.create') }}" class="block text-gray-700 hover:text-indigo-600">
             <i class="fas fa-plus mr-1"></i> Добави лаптоп
         </a>
-       
+        @if(Auth::user()->canAccessPanel(app(\filament\Panel::class)))
         <a href="{{ url('/admin') }}" class="block text-gray-700 hover:text-indigo-600">
             <i class="fas fa-cogs mr-1"></i> Admin
         </a>
+        @endif
         <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button type="submit" class="w-full text-left text-gray-700 hover:text-indigo-600">
